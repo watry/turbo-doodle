9469c5184414003c3f313184093bbce9998fdb6e
watry <135565@gmail.com>
2021-10-26 17:36:01 +0800

    作业
    198

---
+++ assignments/2021282110198/2/AddService.java
@@ -0,0 +1,9 @@
+package ws.example.add;
+
+public class AddService {
+
+	 public double add(double a1,double a2){
+  System.out.println(a1+"+"+a2+"="+(a1+a2));    
+	 return  a1+a2;
+	    }
+}
---
+++ assignments/2021282110198/2/SubService.java
@@ -0,0 +1,9 @@
+package ws.example.sub;
+
+public class SubService {
+	 public double sub(double a1,double a2){
+ System.out.println(a1+"-"+a2+"="+(a1-a2));    
+	 return  a1-a2;
+	    }
+
+}
